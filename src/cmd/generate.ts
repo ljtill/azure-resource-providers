14eb4238c7e6d79a153ba87560c9c986978caa13
Lyon Till <158992+ljtill@users.noreply.github.com>
2022-09-05 17:07:28 +0100

    feat: support command arguments (#12)

--- src/cmd/generate.ts
+++ src/cmd/generate.ts
@@ -1,53 +1,42 @@
 import * as files from '../core/files'
 import * as resources from '../core/resources'
 
-function generateAllFile(): void {
-    // TODO: Implementation
-
-    // Output Path
-    // - Default unless provided via command line argument
-    // ./lib/schema.json
-    // ./lib/specifications.json
-}
-
-function generateSpecificationsFile(repoFilePath: string): void {
-    // TODO: Implementation
-
-    // Output Path
-    // - Default unless provided via command line argument
-    // ./lib/specs.json
-}
-
-function generateSchemasFile(repoFilePath: string): void {
-    // TODO: Arguments
-
-    const schemaFilePaths = files.listSchemaFiles(repoFilePath)
-
+function generateSchemasFile(repoDirPath: string, outFilePath: string): void {
+    const schemaFilePaths = files.listSchemaFiles(repoDirPath)
     const resourceProviders = resources.listResourceProviders(schemaFilePaths)
-    files.writeJsonFile('./schema.json', resourceProviders)
+
+    files.writeJsonFile(outFilePath, resourceProviders)
 }
 
-function main(): void {
+function generateSpecificationsFile(repoDirPath: string, outFilePath: string): void {
+    // TODO: Implementation
+}
+
+function main(argv: string[]): void {
+
+    // TODO: Implement as command line argument
+    const dirPath = "../../github-azure/azure-resource-manager-schemas/schemas"
 
     // Args
     // - generate specs
     // - generate schemas
-    // - generate all
 
-    // TODO: Implement as command line argument
-    const filePath = "../../github-azure/azure-resource-manager-schemas/schemas"
-
-    // Repository path
-    // ./github-azure/azure-rest-api-specs
-    // ./specification
-    // ./github-azure/azure-resource-manager-schemas
-    // ./schemas
-
-    // TODO: Parse command arguments
-    // generateSpecificationsFile(filePath)
-    generateSchemasFile(filePath)
+    switch (argv[2]) {
+        case 'schemas':
+            // TODO: console.log(`[debug] Initiating schemas generation`)
+            // TODO: testRepoPath('./github-azure/azure-rest-api-specs')
+            generateSchemasFile(dirPath, "./schemas.json")
+            break;
+        case 'specs':
+            // TODO: console.log(`[debug] Initiating specs generation`)
+            // TODO: testRepoPath('./github-azure/azure-resource-manager-schemas')
+            generateSpecificationsFile(dirPath, "./specs.json")
+            break;
+        default:
+            console.error("Invalid argument")
+    }
 
     process.exit(0)
 }
 
-main()
+main(process.argv)
