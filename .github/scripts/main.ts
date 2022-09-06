a722ddcc5747682b0bdb144c0d762ad915c58b08
Lyon Till <lyon.till@microsoft.com>
2022-09-06 20:18:17 +0100

    feat: rewrite to deno

---
+++ .github/scripts/main.ts
@@ -0,0 +1,50 @@
+import { listSchemaFiles, listSpecificationFiles } from "./core/files.ts";
+import { listResourceProviders } from "./core/resources.ts";
+import { logger, writeJsonFile } from "./utils.ts";
+
+function generateSchemasFile(filePath: string): void {
+  const dirPath = "../../github-azure/azure-resource-manager-schemas/schemas";
+
+  try {
+    const schemaFilePaths = listSchemaFiles(dirPath);
+    if (schemaFilePaths.length > 0) {
+      writeJsonFile(filePath, listResourceProviders(schemaFilePaths));
+    }
+  } catch (err) {
+    logger.error(err);
+    Deno.exit(1);
+  }
+}
+
+function generateSpecificationsFile(filePath: string): void {
+  const dirPath = "../../github-azure/azure-rest-api-specs/specification";
+
+  try {
+    const specFilePaths = listSpecificationFiles(dirPath);
+    if (specFilePaths.length > 0) {
+      writeJsonFile(filePath, listResourceProviders(specFilePaths));
+    }
+  } catch (err) {
+    logger.error(err);
+    Deno.exit(1);
+  }
+}
+
+function main(args: string[]): Promise<void> {
+  switch (args[0]) {
+    case "schemas":
+      logger.info("Generating schemas file");
+      generateSchemasFile("./schemas.json");
+      break;
+    case "specs":
+      logger.info("Generating specifications file");
+      generateSpecificationsFile("./specs.json");
+      break;
+    default:
+      logger.error("Invalid command argument");
+  }
+
+  Deno.exit(0);
+}
+
+main(Deno.args);
