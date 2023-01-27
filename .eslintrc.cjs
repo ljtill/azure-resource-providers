ab0a7bdd052295a131cbebf1dc8a2572648598b3
Lyon Till <158992+ljtill@users.noreply.github.com>
2023-01-27 10:59:37 +0000

    chore: add recommended type checking (#136)
    * chore: add recommended type checking
    
    * fix: disable any type warning

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,10 +1,19 @@
 module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
+    parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+    },
     plugins: ['@typescript-eslint'],
-    ignorePatterns: ['dist'],
+    ignorePatterns: [
+        'node_modules',
+        'dist',
+        '.eslintrc.cjs'
+    ],
     extends: [
         'eslint:recommended',
         'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
     ],
 };
